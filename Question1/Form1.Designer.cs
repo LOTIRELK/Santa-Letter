26f19227286775166d7e288f9d6d7ea9d2c8fca5
LOTIRELK <lorcan.james.otoole@gmail.com>
2017-06-07 13:03:41 +0100

    jy
    jy

---
+++ Question1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Question1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtBirthDate = new System.Windows.Forms.TextBox();
+            this.txtToy = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(296, 332);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 35);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(18, 332);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(112, 35);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Caclulate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 129);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "D.O.B:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 238);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Top Toy:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(258, 35);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(148, 26);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtBirthDate
+            // 
+            this.txtBirthDate.Location = new System.Drawing.Point(258, 145);
+            this.txtBirthDate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBirthDate.Name = "txtBirthDate";
+            this.txtBirthDate.Size = new System.Drawing.Size(148, 26);
+            this.txtBirthDate.TabIndex = 6;
+            // 
+            // txtToy
+            // 
+            this.txtToy.Location = new System.Drawing.Point(258, 238);
+            this.txtToy.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtToy.Name = "txtToy";
+            this.txtToy.Size = new System.Drawing.Size(148, 26);
+            this.txtToy.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 402);
+            this.Controls.Add(this.txtToy);
+            this.Controls.Add(this.txtBirthDate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnExit);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtBirthDate;
+        private System.Windows.Forms.TextBox txtToy;
+    }
+}
+
---
+++ Question1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Question1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            bool quitflag = true;
+
+            //convert input in textbox to string
+            string childName = Convert.ToString(txtName.Text);
+            string childToy = txtToy.Text;
+            
+            //parse date entered 
+            DateTime childBirthday = DateTime.Parse(txtBirthDate.Text);
+
+            //quitflad is value returned from method
+            quitflag =ValidationCheck(childName);
+
+            //get current date
+            DateTime currentDate = DateTime.Now;
+
+            //create christmas date
+            DateTime christmas = new DateTime(2016, 12, 25);
+
+            //numDays is days till christmas from current date
+            TimeSpan numDays = christmas.Subtract(currentDate);
+            int days = numDays.Days;
+
+            //get total hours till christmas
+            TimeSpan numHours = christmas.Subtract(currentDate);
+            double h = numHours.TotalHours;
+            double hours =Math.Round(h, 0);
+
+            //get age of user - minus the childs birthday from the current date
+            int ages = currentDate.Year- childBirthday.Year;
+             
+            //condition used to cheang the tense of the sentence
+            string condition;
+            
+            //if birthday month is current month
+            if(childBirthday.Month == currentDate.Month)
+            {
+                condition = "is";
+            }
+            else
+                condition = "was";
+            
+            string month = childBirthday.ToString("MMMM");
+
+
+
+
+            //if method has returned true
+            if (quitflag==true)
+            {
+                MessageBox.Show(
+                        "Hi " + childName + "\n\n" +
+
+                        "\t" + "Only " + days + " days to Christmas that's only " + hours + " hours to " + "\n" +
+                        "\t" + "go... wow your birthday " + condition + " in " + month + "...you are" + "\n" +
+                        "\t" + ages + "... see you soon with your " + childToy + "\n\n"
+
+                        + "See you soon ..." + "\n\n"
+
+                        + "Santa"
+
+                        );
+            }
+            
+            
+        }
+        //method for form validation
+        public bool ValidationCheck(string childName)
+        {
+            //if a toy is not entered
+            if (txtToy.Text == "")
+            {
+                MessageBox.Show(
+                "Toy is a required field.",
+                "Entry Error");
+                txtToy.Focus();
+                return false;
+            }
+            //if name not entered
+            if (txtName.Text == "")
+            {
+                MessageBox.Show(
+                "Name a required field.",
+                "Entry Error");
+                txtName.Focus();
+                return false;
+            }
+            //if birthdate is not entered
+            if (txtBirthDate.Text == "")
+            {
+                MessageBox.Show(
+                "Date of birth is a required field.",
+                "Entry Error");
+                txtBirthDate.Focus();
+                return false;
+            }
+            //if a digit is entered in name field
+            foreach (var letter in childName)
+            {
+                bool isDigit = char.IsDigit(letter);
+                if (isDigit)
+                {
+                    MessageBox.Show("Name entered must be a character type."+"\n"+
+                        "e.g. John Smith"+"\n"+
+                    "Entry Error");
+                    txtName.Focus();
+                    return false;
+                }
+            }
+          
+            //default return value
+            return true;
+            
+
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
